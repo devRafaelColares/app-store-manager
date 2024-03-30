5c7aa209d9c89e7e8baed29f13ffdf9fadb6b36f
Rafael Colares <rafaelcolares.dev@gmail.com>
2024-03-30 14:17:15 -0300

    Refact: arrumando arquivo connection.

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql2 = require('mysql2/promise');
 
 const connection = mysql2.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
+  host: process.env.MYSQL_HOSTNAME || 'localhost',
   port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: 'StoreManager',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
   connectionLimit: 10,
   queueLimit: 0,
   waitForConnections: true,
